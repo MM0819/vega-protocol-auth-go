6e1ddbbf084319543a45d901e0cfddd9ef2f584c
MM0819 <MM0819@protonmail.com>
2023-03-06 23:57:54 +0000

    comments

--- main.go
+++ main.go
@@ -12,9 +12,13 @@ const CoreNode = "n08.testnet.vega.xyz:3002"
 const ProposalId = "0f4d06000087b989f613bf3a651842b88874d70c4b8b3161c7257837447c3400"
 
 func main() {
+	// create a wallet using your bip39 mnemonic
 	wallet := auth.NewWallet(Mnemonic)
+	// instantiate the authenticator
 	authenticator := auth.NewAuthenticator(CoreNode, wallet)
+	// select a signing key
 	partyId := wallet.Get(0).PublicKey
+	// build the input data for your command
 	inputData := &commandspb.InputData{
 		Command: &commandspb.InputData_VoteSubmission{
 			VoteSubmission: &commandspb.VoteSubmission{
@@ -23,8 +27,10 @@ func main() {
 			},
 		},
 	}
+	// sign the request
 	tx := authenticator.Sign(partyId, inputData)
 	log.Printf("%v", tx)
+	// submit your transaction
 	resp := authenticator.SubmitTx(tx)
 	log.Printf("%v", resp)
 }
