dcc5f0b1a908583d2adba549a1a73c2acf347d2a
MM0819 <MM0819@protonmail.com>
2023-04-06 18:41:41 +0100

    parallel pow

--- auth/auth.go
+++ auth/auth.go
@@ -9,21 +9,179 @@ import (
 	"context"
 	"crypto/ed25519"
 	"encoding/hex"
+	"fmt"
 	"github.com/google/uuid"
+	"github.com/sasha-s/go-deadlock"
 	"golang.org/x/crypto/sha3"
+	"golang.org/x/exp/maps"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"log"
+	"math"
 	"math/rand"
+	"sort"
+	"sync"
+	"time"
 )
 
+const NumberOfPastBlocks = 100
+const TxPerBlock = 2
+
+type ProofOfWork struct {
+	BlockHash   string
+	BlockHeight uint64
+	Difficulty  uint
+	Nonce       uint64
+	TxId        string
+	Used        bool
+}
+
+func NewProofOfWork(
+	blockHash string,
+	blockHeight uint64,
+	difficulty uint,
+	nonce uint64,
+	txId string,
+	used bool,
+) *ProofOfWork {
+	return &ProofOfWork{
+		BlockHash:   blockHash,
+		BlockHeight: blockHeight,
+		Difficulty:  difficulty,
+		Nonce:       nonce,
+		TxId:        txId,
+		Used:        used,
+	}
+}
+
 type Authenticator struct {
-	coreNode string
-	wallet   *Wallet
+	coreNode   string
+	wallet     *Wallet
+	mu         deadlock.RWMutex
+	powByBlock map[uint64][]*ProofOfWork
 }
 
 func NewAuthenticator(coreNode string, wallet *Wallet) *Authenticator {
-	return &Authenticator{coreNode: coreNode, wallet: wallet}
+	authenticator := &Authenticator{
+		coreNode:   coreNode,
+		wallet:     wallet,
+		powByBlock: map[uint64][]*ProofOfWork{},
+	}
+	go func() {
+		for range time.NewTicker(time.Second).C {
+			authenticator.computeProofOfWork()
+		}
+	}()
+	go func() {
+		for range time.NewTicker(time.Second).C {
+			authenticator.removeOldProofOfWork()
+		}
+	}()
+	return authenticator
+}
+
+func (a *Authenticator) removeOldProofOfWork() {
+	lastBlock := a.getLastBlock()
+	if lastBlock == nil {
+		return
+	}
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	oldestBlock := lastBlock.Height - uint64(math.Round(0.8*NumberOfPastBlocks))
+	for _, height := range maps.Keys(a.powByBlock) {
+		if height <= oldestBlock {
+			delete(a.powByBlock, height)
+		}
+	}
+}
+
+func (a *Authenticator) computeProofOfWork() {
+	lastBlock := a.getLastBlock()
+	if lastBlock == nil {
+		return
+	}
+	a.mu.Lock()
+	if ok := a.powByBlock[lastBlock.Height]; ok == nil {
+		a.powByBlock[lastBlock.Height] = make([]*ProofOfWork, 0)
+	}
+	powCount := len(a.powByBlock[lastBlock.Height])
+	a.mu.Unlock()
+	if powCount == 0 {
+		total := 10.0
+		var wg sync.WaitGroup
+		for i := 0; i < int(total); i++ {
+			wg.Add(1)
+			i := i
+			go func() {
+				difficulty := uint(lastBlock.GetSpamPowDifficulty())
+				extraZeroes := uint(math.Floor(float64(i+1) / TxPerBlock))
+				difficulty = difficulty + extraZeroes
+				txId, _ := uuid.NewRandom()
+				nonce, _, _ := crypto.PoW(lastBlock.Hash, txId.String(), difficulty, lastBlock.SpamPowHashFunction)
+				pow := NewProofOfWork(
+					lastBlock.Hash, lastBlock.Height, difficulty, nonce, txId.String(), false,
+				)
+				a.mu.Lock()
+				a.powByBlock[lastBlock.Height] = append(a.powByBlock[lastBlock.Height], pow)
+				a.mu.Unlock()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+		fmt.Printf("computed %f PoW\n", total)
+	}
+}
+
+func (a *Authenticator) HasProofOfWork() bool {
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	var result = false
+	for _, height := range maps.Keys(a.powByBlock) {
+		for _, pow := range a.powByBlock[height] {
+			if !pow.Used {
+				result = true
+				break
+			}
+		}
+	}
+	return result
+}
+
+func (a *Authenticator) taintProofOfWork(blockHeight uint64, txId string) {
+	powList := a.powByBlock[blockHeight]
+	if powList != nil {
+		for i, pow := range powList {
+			if pow.TxId == txId {
+				powList[i].Used = true
+			}
+		}
+	}
+}
+
+func (a *Authenticator) getProofOfWork() *ProofOfWork {
+	powList := make([]*ProofOfWork, 0)
+	for _, height := range maps.Keys(a.powByBlock) {
+		for _, pow := range a.powByBlock[height] {
+			if !pow.Used {
+				powList = append(powList, pow)
+			}
+		}
+	}
+	sort.Slice(powList, func(i, j int) bool {
+		if powList[i].BlockHeight == powList[j].BlockHeight {
+			return powList[i].Difficulty < powList[j].Difficulty
+		}
+		return powList[i].BlockHeight < powList[j].BlockHeight
+	})
+	if len(powList) == 0 {
+		fmt.Println("waiting for proof of work...")
+		time.Sleep(time.Second)
+		return a.getProofOfWork()
+	} else {
+		pow := powList[0]
+		a.taintProofOfWork(pow.BlockHeight, pow.TxId)
+		return pow
+	}
 }
 
 func (a *Authenticator) getLastBlock() *corepb.LastBlockHeightResponse {
@@ -33,7 +191,7 @@ func (a *Authenticator) getLastBlock() *corepb.LastBlockHeightResponse {
 	defer coreNode.Close()
 	resp, err := coreService.LastBlockHeight(context.Background(), req)
 	if err != nil {
-		log.Printf("couldn't get last block: %v", err)
+		log.Printf("couldn't get last block: %v\n", err)
 		return nil
 	}
 	return resp
@@ -44,11 +202,14 @@ func (a *Authenticator) buildTx(
 	lastBlock *corepb.LastBlockHeightResponse,
 	inputData *commandspb.InputData,
 ) *commandspb.Transaction {
-	pow := a.getProofOfWork(lastBlock)
+	a.mu.Lock()
+	a.mu.Unlock()
+	pow := a.getProofOfWork()
 	if pow == nil {
-		log.Printf("couldn't get proof of work")
+		log.Println("couldn't get proof of work")
 		return nil
 	}
+	inputData.BlockHeight = pow.BlockHeight
 	inputDataBytes, _ := proto.Marshal(inputData)
 	inputDataPacked := bytes.Join([][]byte{
 		[]byte(lastBlock.ChainId),
@@ -61,10 +222,11 @@ func (a *Authenticator) buildTx(
 		Version: 1,
 		Value:   hexSig,
 	}
+	proofOfWork := &commandspb.ProofOfWork{Tid: pow.TxId, Nonce: pow.Nonce}
 	tx := &commandspb.Transaction{
 		Version:   commandspb.TxVersion_TX_VERSION_V3,
 		Signature: signature,
-		Pow:       pow,
+		Pow:       proofOfWork,
 		InputData: inputDataBytes,
 		From:      &commandspb.Transaction_PubKey{PubKey: keyPair.PublicKey},
 	}
@@ -81,13 +243,6 @@ func (a *Authenticator) signInputData(privateKey string, inputDataPacked []byte)
 	return hex.EncodeToString(sig)
 }
 
-func (a *Authenticator) getProofOfWork(lastBlock *corepb.LastBlockHeightResponse) *commandspb.ProofOfWork {
-	difficulty := uint(lastBlock.GetSpamPowDifficulty())
-	txId, _ := uuid.NewRandom()
-	nonce, _, _ := crypto.PoW(lastBlock.Hash, txId.String(), difficulty, lastBlock.SpamPowHashFunction)
-	return &commandspb.ProofOfWork{Tid: txId.String(), Nonce: nonce}
-}
-
 func (a *Authenticator) Sign(partyId string, inputData *commandspb.InputData) *commandspb.Transaction {
 	lastBlock := a.getLastBlock()
 	if lastBlock == nil {
@@ -97,7 +252,7 @@ func (a *Authenticator) Sign(partyId string, inputData *commandspb.InputData) *c
 	inputData.Nonce = rand.Uint64()
 	keyPair, err := a.wallet.GetByPublicKey(partyId)
 	if err != nil {
-		log.Printf("%v", err)
+		log.Printf("%v\n", err)
 		return nil
 	}
 	return a.buildTx(keyPair, lastBlock, inputData)
@@ -110,9 +265,9 @@ func (a *Authenticator) SubmitTx(tx *commandspb.Transaction) *corepb.SubmitTrans
 	defer coreNode.Close()
 	resp, err := coreService.SubmitTransaction(context.Background(), req)
 	if err != nil {
-		log.Printf("couldn't submit tx: %v", err)
+		log.Printf("couldn't submit tx: %v\n", err)
 	} else if !resp.Success {
-		log.Printf("tx = %s; code = %d; data = %s", resp.TxHash, resp.Code, resp.Data)
+		log.Printf("tx = %s; code = %d; data = %s\n", resp.TxHash, resp.Code, resp.Data)
 	}
 	return resp
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	code.vegaprotocol.io/vega v0.68.3
 	github.com/google/uuid v1.3.0
+	github.com/sasha-s/go-deadlock v0.3.1
 	github.com/tyler-smith/go-bip39 v1.1.0
 	github.com/vegaprotocol/go-slip10 v0.1.0
 	github.com/vitelabs/go-vite v2.10.2+incompatible
@@ -19,6 +20,7 @@ require (
 	github.com/go-errors/errors v1.4.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.9.0 // indirect
+	github.com/petermattis/goid v0.0.0-20180202154549-b0b1615b78e5 // indirect
 	github.com/stretchr/testify v1.8.2 // indirect
 	golang.org/x/net v0.8.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
